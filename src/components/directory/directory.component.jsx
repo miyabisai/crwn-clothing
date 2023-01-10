49a36b3bbbb9c90266c2e4527102ea40c1001344
miyabisai <guirec0417@gmail.com>
2023-01-10 21:25:02 +0900

    fix css bug

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,6 @@
-import CategoryItem from '../category-item/category-item.component';
+// import CategoryItem from '../category-item/category-item.component';
+
+import DirectoryItem from '../directory-item/directory-item.component';
 import './directory.styles.scss';
 
 const Directory = ({categories})=>{
@@ -6,7 +8,8 @@ const Directory = ({categories})=>{
         <div className='categories-container'>
         {
           categories.map((category)=>(
-            <CategoryItem key={category.id} category={category}/>
+            // <CategoryItem key={category.id} category={category}/>
+            <DirectoryItem key={category.id} category={category}/>
           ))
         }
         </div>
